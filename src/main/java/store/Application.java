74ab7f54bf917592cccaaf9cfd2a9c8481e838ef
권장순 <jsoonworld@gmail.com>
2024-11-07 16:17:32 +0900

    feat(OutputView) : 환영 메시지를 출력하는 기능 추가
    - WELCOME_MESSAGE 상수를 통해 메시지 관리
    - printWelcomeMessage 메서드로 환영 메시지 출력

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -7,28 +7,33 @@ import store.repository.Repository;
 import store.repository.ProductFileRepository;
 import store.repository.PromotionFileRepository;
 import store.util.FilePathLoader;
+import store.view.OutputView;
 
 import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            FileDataLoader productLoader = new ProductDataLoader();
-            FileDataLoader promotionLoader = new PromotionDataLoader();
 
-            String productFilePath = FilePathLoader.getResourceFilePath("products.md");
-            String promotionFilePath = FilePathLoader.getResourceFilePath("promotions.md");
+        OutputView outputView = new OutputView();
+        outputView.printWelcomeMessage();
 
-            Repository<String> productRepository = new ProductFileRepository(productLoader, productFilePath);
-            Repository<String> promotionRepository = new PromotionFileRepository(promotionLoader, promotionFilePath);
-
-            System.out.println("Products:");
-            productRepository.findAll().forEach(System.out::println);
-
-            System.out.println("\nPromotions:");
-            promotionRepository.findAll().forEach(System.out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            FileDataLoader productLoader = new ProductDataLoader();
+//            FileDataLoader promotionLoader = new PromotionDataLoader();
+//
+//            String productFilePath = FilePathLoader.getResourceFilePath("products.md");
+//            String promotionFilePath = FilePathLoader.getResourceFilePath("promotions.md");
+//
+//            Repository<String> productRepository = new ProductFileRepository(productLoader, productFilePath);
+//            Repository<String> promotionRepository = new PromotionFileRepository(promotionLoader, promotionFilePath);
+//
+//            System.out.println("Products:");
+//            productRepository.findAll().forEach(System.out::println);
+//
+//            System.out.println("\nPromotions:");
+//            promotionRepository.findAll().forEach(System.out::println);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,9 @@
+package store.view;
+
+public class OutputView {
+    private static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.";
+
+    public void printWelcomeMessage() {
+        System.out.println(WELCOME_MESSAGE);
+    }
+}
