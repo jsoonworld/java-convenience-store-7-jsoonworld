72a01d55f7c5af2cc3b407e0d06e31268ee73f84
권장순 <jsoonworld@gmail.com>
2024-11-08 17:29:43 +0900

    feat(Product) : Product 클래스 및 테스트 구현
    - Product 클래스에서 상품명, 가격, 수량, 프로모션 정보를 관리
    - 프로모션 유무, 재고 상태 확인 기능 추가
    - ProductTest 클래스에서 객체 생성, 프로모션 및 재고 상태 확인 테스트 구현

---
+++ src/main/java/store/domain/Product.java
@@ -0,0 +1,82 @@
+package store.domain;
+
+import store.domain.vo.ProductName;
+import store.domain.vo.Price;
+import store.domain.vo.PromotionName;
+import store.domain.vo.Quantity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Optional;
+
+public class Product {
+    private final ProductName productName;
+    private final Price price;
+    private final Quantity quantity;
+    private final PromotionName promotionName;
+
+    private Product(ProductName productName, Price price, Quantity quantity, PromotionName promotionName) {
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotionName = promotionName;
+    }
+
+    public static Product of(ProductName productName, Price price, Quantity quantity, PromotionName promotionName) {
+        return new Product(productName, price, quantity, promotionName);
+    }
+
+    public String getName() {
+        return productName.getName();
+    }
+
+    public int getPriceValue() {
+        return price.getValue();
+    }
+
+    public int getQuantityValue() {
+        return quantity.getAmount();
+    }
+
+    public boolean hasNoStock() {
+        return quantity.isOutOfStock();
+    }
+
+    public boolean hasPromotion() {
+        return !promotionName.isNullPromotion();
+    }
+
+    public String getPromotionName() {
+        return promotionName.value();
+    }
+
+    public Optional<Promotion> getActivePromotion(Promotions promotions, LocalDate date) {
+        if (hasPromotion()) {
+            return findActivePromotion(promotions, date);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Product product)) {
+            return false;
+        }
+        return productName.equals(product.productName) &&
+                price.equals(product.price) &&
+                quantity.equals(product.quantity) &&
+                promotionName.equals(product.promotionName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productName, price, quantity, promotionName);
+    }
+
+    private Optional<Promotion> findActivePromotion(Promotions promotions, LocalDate date) {
+        return promotions.findActivePromotion(promotionName.value(), date);
+    }
+}
