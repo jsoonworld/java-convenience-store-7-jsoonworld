c716e76a06cca2f9124ab884397d950f8b983b52
권장순 <jsoonworld@gmail.com>
2024-11-08 17:33:17 +0900

    feat(Products) : 프로모션 대상 상품 필터링 기능 추가 및 테스트 구현
    - Products 클래스 추가로 상품 리스트 관리 및 프로모션 적용 상품 필터링 기능 구현
    - ProductsTest에서 프로모션이 있는 상품만 필터링하여 반환하는 테스트 케이스 추가

---
+++ src/main/java/store/domain/Products.java
@@ -0,0 +1,18 @@
+package store.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Products {
+    private final List<Product> products;
+
+    public Products(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Product> findProductsOnPromotion() {
+        return products.stream()
+                .filter(Product::hasPromotion)
+                .collect(Collectors.toList());
+    }
+}
