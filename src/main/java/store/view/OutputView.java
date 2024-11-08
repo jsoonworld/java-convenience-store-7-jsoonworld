891b6e71ff3a10cf6a837f9331acc072ededeb27
권장순 <jsoonworld@gmail.com>
2024-11-08 21:11:37 +0900

    feat(OutputView) : 상품 구매 입력 안내 메시지 출력 기능 구현
    - printPurchasePrompt 메서드를 추가하여 사용자에게 상품명과 수량 입력 안내 메시지 출력
    - 상수 PURCHASE_PROMPT 추가로 안내 메시지 내용 관리

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class OutputView {
     private static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.";
     private static final String CURRENT_PRODUCTS_MESSAGE = "현재 보유하고 있는 상품입니다.";
+    private static final String PURCHASE_PROMPT = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
 
     public void printWelcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
@@ -17,5 +18,10 @@ public class OutputView {
     public void printProducts(List<String> formattedProducts) {
         System.out.println();
         formattedProducts.forEach(System.out::println);
+    }
+
+    public void printPurchasePrompt() {
+        System.out.println();
+        System.out.println(PURCHASE_PROMPT);
     }
 }
