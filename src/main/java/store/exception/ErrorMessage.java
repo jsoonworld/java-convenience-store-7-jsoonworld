cab5216d2a8f0809dda1733a0416b27c66a8073d
권장순 <jsoonworld@gmail.com>
2024-11-08 21:05:14 +0900

    feat(ErrorMessage) : 에러 메시지 추가 구현
    - 상품 이름, 가격, 수량, 프로모션 유효성 검사에 대한 에러 메시지 추가
    - 프로모션 날짜 및 파일 로드 실패 관련 에러 메시지 추가
    - 각 예외 상황에 맞는 사용자 친화적인 에러 메시지로 업데이트

--- src/main/java/store/exception/ErrorMessage.java
+++ src/main/java/store/exception/ErrorMessage.java
@@ -2,7 +2,24 @@ package store.exception;
 
 public enum ErrorMessage {
     FILE_NOT_FOUND("[ERROR] 해당 파일을 찾을 수 없습니다."),
-    FILE_READ_ERROR("[ERROR] 파일을 읽을 수 없습니다.");
+    FILE_READ_ERROR("[ERROR] 파일을 읽을 수 없습니다."),
+
+    EMPTY_PRODUCT_NAME("상품 이름은 비어 있을 수 없습니다. 유효한 이름을 입력해 주세요."),
+    NULL_PRODUCT_NAME("상품 이름은 null일 수 없습니다."),
+    NULL_PRICE("가격은 null일 수 없습니다."),
+    NEGATIVE_PRICE("가격은 0 이상이어야 합니다. 음수는 허용되지 않습니다."),
+    NULL_QUANTITY("수량은 null일 수 없습니다."),
+    NEGATIVE_QUANTITY("수량은 0 이상이어야 합니다. 음수는 허용되지 않습니다."),
+    INVALID_PROMOTION_NAME("프로모션 이름은 null일 수 없습니다."),
+
+    FILE_LOAD_FAILURE("파일 데이터를 로드하는 데 실패했습니다."),
+
+    NEGATIVE_FREE_QUANTITY("무료 제공 수량은 음수일 수 없습니다."),
+    INVALID_PROMOTION_PERIOD("프로모션 시작 날짜는 종료 날짜 이후일 수 없습니다."),
+    NEGATIVE_PROMOTION_QUANTITY("프로모션 수량은 음수일 수 없습니다."),
+
+    NULL_START_DATE("프로모션 시작 날짜는 null일 수 없습니다."),
+    NULL_END_DATE("프로모션 종료 날짜는 null일 수 없습니다.");
 
     private final String message;
 
