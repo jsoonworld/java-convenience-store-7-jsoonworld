10522791e9cd785e2ad01c2fc0c4134a093d85e2
권장순 <jsoonworld@gmail.com>
2024-11-09 23:32:46 +0900

    refactor(ProductController) : ProductRepository에서 직접 제품 목록 가져오도록 변경
    - ProductController가 ProductRepository에서 직접 제품 목록을 가져와 포맷하도록 수정
    - ProductCsvParser 의존성 제거로 코드 간소화 및 데이터 흐름 명확화
    - getFormattedProducts 메서드에서 로딩된 Product 객체를 바로 포맷하도록 변경

--- src/main/java/store/controller/ProductController.java
+++ src/main/java/store/controller/ProductController.java
@@ -2,7 +2,6 @@ package store.controller;
 
 import store.domain.Product;
 import store.repository.ProductRepository;
-import store.util.ProductCsvParser;
 import store.util.ProductInventoryFormatter;
 
 import java.util.List;
@@ -10,16 +9,13 @@ import java.util.List;
 public class ProductController {
 
     private final ProductRepository repository;
-    private final ProductCsvParser parser;
 
-    public ProductController(ProductRepository repository, ProductCsvParser parser) {
+    public ProductController(ProductRepository repository) {
         this.repository = repository;
-        this.parser = parser;
     }
 
     public List<String> getFormattedProducts() {
-        List<String> lines = repository.loadAll();
-        List<Product> products = parser.parseProducts(lines);
+        List<Product> products = repository.getAllProducts();
         return ProductInventoryFormatter.format(products);
     }
 }
