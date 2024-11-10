edef60ef387a1e9f5499fd99865a2ceb89699e7a
권장순 <jsoonworld@gmail.com>
2024-11-10 16:50:30 +0900

    refactor(FilePathLoader) : 파일 경로 로딩 및 복사 로직 개선
    - 파일 경로를 로딩하고 파일이 존재하지 않으면 지정된 경로에 복사하는 로직을 개선.
    - `getWritableFilePath` 메서드에서 파일을 복사하는 과정에서 디렉토리 생성 및 파일 덮어쓰기 처리 추가.
    - 리소스 파일 경로를 확인하는 `getResourceFilePath` 메서드에서 파일 경로 유효성 검사 개선.

--- src/main/java/store/util/FilePathLoader.java
+++ src/main/java/store/util/FilePathLoader.java
@@ -2,10 +2,25 @@ package store.util;
 
 import static store.exception.ErrorMessage.*;
 
+import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class FilePathLoader {
+
+    public static String getWritableFilePath(String fileName) throws IOException {
+        String resourcePath = getResourceFilePath(fileName);
+        Path sourcePath = Paths.get(resourcePath);
+        Path targetPath = Paths.get("src/main/modifiable-data/" + fileName);
+
+        Files.createDirectories(targetPath.getParent());
+
+        if (!Files.exists(targetPath)) {
+            Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        return targetPath.toString();
+    }
 
     public static String getResourceFilePath(String fileName) {
         URL resource = validateFileExists(fileName);
