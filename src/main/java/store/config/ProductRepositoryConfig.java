ccd25202154809afa5ddea63c537563eeb254565
권장순 <jsoonworld@gmail.com>
2024-11-10 16:27:49 +0900

    refactor(ProductRepositoryConfig) : ProductRepository 초기화 로직 개선 및 ProductStockSaver 주입 방식 수정
     ProductRepositoryConfig에서 ProductStockSaver를 인자로 받아 초기화 시 사용하도록 수정.
    - ProductRepository의 초기화 시, ProductStockSaver가 필요한 경우에 맞게 반환 객체 수정.

--- src/main/java/store/config/ProductRepositoryConfig.java
+++ src/main/java/store/config/ProductRepositoryConfig.java
@@ -3,7 +3,8 @@ package store.config;
 import static store.exception.ErrorMessage.*;
 
 import store.domain.Product;
-import store.io.FileDataLoader;
+import store.io.input.FileDataLoader;
+import store.io.output.ProductStockSaver;
 import store.repository.ProductRepository;
 import store.util.ProductCsvParser;
 
@@ -14,18 +15,20 @@ public class ProductRepositoryConfig {
     private final FileDataLoader dataLoader;
     private final ProductCsvParser parser;
     private final String filePath;
+    private final ProductStockSaver productStockSaver;
 
-    public ProductRepositoryConfig(FileDataLoader dataLoader, ProductCsvParser parser, String filePath) {
+    public ProductRepositoryConfig(FileDataLoader dataLoader, ProductCsvParser parser, String filePath, ProductStockSaver productStockSaver) {
         this.dataLoader = dataLoader;
         this.parser = parser;
         this.filePath = filePath;
+        this.productStockSaver = productStockSaver;
     }
 
     public ProductRepository initializeProductRepository() {
         try {
             List<String> rawData = dataLoader.loadFile(filePath);
             List<Product> parsedProducts = parser.parseProducts(rawData);
-            return new ProductRepository(parsedProducts);
+            return new ProductRepository(parsedProducts, productStockSaver);
         } catch (IOException e) {
             throw new RuntimeException(PRODUCT_DATA_INITIALIZATION_ERROR.getMessage());
         }
