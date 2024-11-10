b35bd688768192f62b7c4f7cb32805aad4d44da6
권장순 <jsoonworld@gmail.com>
2024-11-11 04:11:03 +0900

    refactor(ProductRepositoryConfig) : 초기화 로직 수정

--- src/main/java/store/config/ProductRepositoryConfig.java
+++ src/main/java/store/config/ProductRepositoryConfig.java
@@ -4,7 +4,6 @@ import static store.exception.ErrorMessage.*;
 
 import store.domain.Product;
 import store.io.input.FileDataLoader;
-import store.io.output.ProductStockSaver;
 import store.repository.ProductRepository;
 import store.util.ProductCsvParser;
 
@@ -15,20 +14,18 @@ public class ProductRepositoryConfig {
     private final FileDataLoader dataLoader;
     private final ProductCsvParser parser;
     private final String filePath;
-    private final ProductStockSaver productStockSaver;
 
-    public ProductRepositoryConfig(FileDataLoader dataLoader, ProductCsvParser parser, String filePath, ProductStockSaver productStockSaver) {
+    public ProductRepositoryConfig(FileDataLoader dataLoader, ProductCsvParser parser, String filePath) {
         this.dataLoader = dataLoader;
         this.parser = parser;
         this.filePath = filePath;
-        this.productStockSaver = productStockSaver;
     }
 
     public ProductRepository initializeProductRepository() {
         try {
             List<String> rawData = dataLoader.loadFile(filePath);
             List<Product> parsedProducts = parser.parseProducts(rawData);
-            return new ProductRepository(parsedProducts, productStockSaver);
+            return new ProductRepository(parsedProducts);
         } catch (IOException e) {
             throw new RuntimeException(PRODUCT_DATA_INITIALIZATION_ERROR.getMessage());
         }
