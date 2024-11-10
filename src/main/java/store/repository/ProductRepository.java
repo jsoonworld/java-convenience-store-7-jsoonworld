48d21d5886721d33b7ec6d1bd8bcd71df96acf3b
권장순 <jsoonworld@gmail.com>
2024-11-10 16:49:25 +0900

    feat(ProductRepository) : 상품 수정 및 저장 기능 구현
    - ProductRepository 클래스에 상품 정보 수정 및 저장 기능 추가.
    - 상품 이름과 프로모션 이름으로 상품을 찾고, 존재하는 상품을 업데이트한 후 파일에 저장.
    - ProductStockSaver를 이용해 변경된 상품 리스트를 파일에 저장.
    - 상품이 없으면 예외를 발생시켜 처리.

--- src/main/java/store/repository/ProductRepository.java
+++ src/main/java/store/repository/ProductRepository.java
@@ -1,16 +1,53 @@
 package store.repository;
 
+import static store.exception.ErrorMessage.*;
+
 import store.domain.Product;
+import store.io.output.ProductStockSaver;
+
 import java.util.List;
 
 public class ProductRepository {
-    private final List<Product> products;
+    private static final int PRODUCT_NOT_FOUND_INDEX = -1;
 
-    public ProductRepository(List<Product> products) {
+    private final List<Product> products;
+    private final ProductStockSaver productStockSaver;
+
+    public ProductRepository(List<Product> products, ProductStockSaver productStockSaver) {
         this.products = products;
+        this.productStockSaver = productStockSaver;
     }
 
     public List<Product> getAllProducts() {
         return products;
+    }
+
+    public Product findProductByName(String productName) {
+        return products.stream()
+                .filter(product -> product.getName().equals(productName))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Product findByNameAndPromotion(String productName, String promotionName) {
+        return products.stream()
+                .filter(product -> product.getName().equals(productName) &&
+                        (product.getPromotionName().equals(promotionName) || promotionName.equals("null")))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void save(Product updatedProduct) {
+        Product existingProduct = findByNameAndPromotion(updatedProduct.getName(), updatedProduct.getPromotionName());
+
+        if (existingProduct == null) {
+            throw new RuntimeException(PRODUCT_SAVE_FAILED.getMessage());
+        }
+
+        int index = products.indexOf(existingProduct);
+        if (index != PRODUCT_NOT_FOUND_INDEX) {
+            products.set(index, updatedProduct);
+        }
+        productStockSaver.saveProductStock(products);
     }
 }
