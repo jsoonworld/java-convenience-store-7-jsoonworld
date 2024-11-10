6c802a077bcc9a2ec8d9dbc91f5447b114472c71
권장순 <jsoonworld@gmail.com>
2024-11-11 04:14:38 +0900

    del(Output): md 파일 수정 방지, 변경사항을 파일에 저장하지 않음
    - 파일에 대한 변경사항을 저장하지 않도록 수정하여 md 파일이 수정되지 않도록 함
    - 이로 인해 파일의 원본 내용이 변경되지 않으며, 변경된 데이터는 별도로 처리됨

--- src/main/java/store/io/output/FileDataSaver.java
+++
@@ -1,8 +0,0 @@
-package store.io.output;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface FileDataSaver {
-    void saveFile(String filePath, List<String> lines) throws IOException;
-}
