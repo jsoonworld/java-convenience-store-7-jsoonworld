1a2f5ed78044374b093cf8bdd1b33a33bce6cd80
권장순 <jsoonworld@gmail.com>
2024-11-11 19:56:08 +0900

    feat(result): 결과 관련 DTO 클래스 구현
    - `PromotionPurchaseRequest`: 프로모션 구매 요청을 위한 클래스. 상품명과 요청 수량 포함.
    - `PromotionResult`: 프로모션 처리 결과를 담는 클래스. 남은 수량, 프로모션 할인액, 저장할 상품 목록, 프로모션 세부 정보 포함.
    - `PurchaseProcessingResult`: 구매 처리 결과를 담는 클래스. 총 가격, 프로모션 할인액, 최종 구매 수량, 구매 및 프로모션 세부 정보, 저장할 상품 목록 포함.
    - `StockResult`: 재고 처리 결과를 담는 클래스. 총 프로모션 할인액과 저장할 상품 목록 포함.

---
+++ src/main/java/store/dto/response/StockResult.java
@@ -0,0 +1,27 @@
+package store.dto.response;
+
+import store.domain.Product;
+
+import java.util.List;
+
+public class StockResult {
+    private final int totalPromotionalDiscount;
+    private final List<Product> productsToSave;
+
+    private StockResult(int totalPromotionalDiscount, List<Product> productsToSave) {
+        this.totalPromotionalDiscount = totalPromotionalDiscount;
+        this.productsToSave = productsToSave;
+    }
+
+    public static StockResult of(int totalPromotionalDiscount, List<Product> productsToSave) {
+        return new StockResult(totalPromotionalDiscount, productsToSave);
+    }
+
+    public int getTotalPromotionalDiscount() {
+        return totalPromotionalDiscount;
+    }
+
+    public List<Product> getProductsToSave() {
+        return productsToSave;
+    }
+}
