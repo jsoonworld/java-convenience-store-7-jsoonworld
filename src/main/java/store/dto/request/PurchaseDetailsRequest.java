49821ded876e358f12f1279ecf59a12383039468
권장순 <jsoonworld@gmail.com>
2024-11-11 19:55:14 +0900

    feat(request): 요청 관련 DTO 클래스 구현
    - `ProductSaveRequest`: 상품 저장 시 요청 정보를 담는 클래스.
    - `PromotionRequest`: 프로모션 적용을 위한 요청 정보를 담는 클래스. 상품명, 요청 수량, 가격, 프로모션 정보 포함.
    - `PurchaseDetailsRequest`: 구매 상세 내역을 요청하기 위한 클래스. 상품명, 수량, 총 가격 포함.
    - `PurchaseProductRequest`: 구매 요청 시 필요한 정보를 담고 있으며 `PurchaseProduct`로 변환 가능.
    - `ReceiptRequest`: 영수증 요청 시 필요한 데이터를 담는 클래스. 총 구매 내역, 최종 수량, 총 가격, 행사 할인, 멤버십 할인 여부 포함.
    - `StockRequest`: 재고 처리 시 필요한 데이터를 담는 클래스. 상품명, 남은 수량, 가격, 요청 수량 포함.

---
+++ src/main/java/store/dto/request/PurchaseDetailsRequest.java
@@ -0,0 +1,31 @@
+package store.dto.request;
+
+import store.domain.Product;
+
+public class PurchaseDetailsRequest {
+    private final String productName;
+    private final int quantity;
+    private final int totalProductPrice;
+
+    private PurchaseDetailsRequest(String productName, int quantity, int totalProductPrice) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.totalProductPrice = totalProductPrice;
+    }
+
+    public static PurchaseDetailsRequest of(Product product, int quantity, int productPrice) {
+        return new PurchaseDetailsRequest(product.getName(), quantity, productPrice * quantity);
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getTotalProductPrice() {
+        return totalProductPrice;
+    }
+}
