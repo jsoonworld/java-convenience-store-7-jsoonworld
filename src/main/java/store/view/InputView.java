36c624f7fb0c3947000f70fcbab675944c600e7b
권장순 <jsoonworld@gmail.com>
2024-11-11 19:42:50 +0900

    feat(InputView) : 멤버십 할인, 추가 구매, 프로모션 확인 입력 추가
    - `InputView`에 멤버십 할인 여부, 추가 구매 여부, 프로모션 혜택 확인 입력 메서드 추가
    - 사용자가 각 단계에서 입력을 통해 해당 정보를 입력할 수 있도록 보완

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,12 +1,30 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import store.dto.PurchaseInput;
+import store.dto.request.AdditionalPurchaseInput;
+import store.dto.request.MembershipDiscountInput;
+import store.dto.request.PromotionConfirmationInput;
+import store.dto.request.PurchaseInput;
 
 public class InputView {
 
     public PurchaseInput readPurchaseInput() {
         String input = Console.readLine();
         return PurchaseInput.from(input);
+    }
+
+    public MembershipDiscountInput readMembershipDiscount() {
+        String membershipInput = Console.readLine();
+        return MembershipDiscountInput.from(membershipInput);
+    }
+
+    public PromotionConfirmationInput readPromotionConfirmation() {
+        String userResponse = Console.readLine();
+        return PromotionConfirmationInput.from(userResponse);
+    }
+
+    public AdditionalPurchaseInput readAdditionalPurchaseInput() {
+        String input = Console.readLine();
+        return AdditionalPurchaseInput.from(input);
     }
 }
