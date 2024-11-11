e9b43fcd1d1277885ac0ec807cb3fcd9c333b93b
권장순 <jsoonworld@gmail.com>
2024-11-11 10:34:24 +0900

    feat(OutputView): 멤버십 할인 여부 안내 메시지 구현
    - 멤버십 할인을 받을지 묻는 `printMembershipPrompt` 메서드 추가
    - 사용자가 할인 여부를 쉽게 선택할 수 있도록 안내 메시지 제공

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
     private static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.";
     private static final String CURRENT_PRODUCTS_MESSAGE = "현재 보유하고 있는 상품입니다.";
     private static final String PURCHASE_PROMPT = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String MEMBERSHIP_PROMPT = "멤버십 할인을 받으시겠습니까? (Y/N)";
 
     public void printWelcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
@@ -23,5 +24,9 @@ public class OutputView {
     public void printPurchasePrompt() {
         System.out.println();
         System.out.println(PURCHASE_PROMPT);
+    }
+
+    public void printMembershipPrompt() {
+        System.out.println(MEMBERSHIP_PROMPT);
     }
 }
