0c7e7b6d08c5f820a60f0dc0a338baacba98265f
권장순 <jsoonworld@gmail.com>
2024-11-11 20:03:45 +0900

    refactor(Application): StoreConfig를 사용하여 StoreController 초기화 및 실행 구조 개선
    - StoreConfig를 통해 StoreController의 초기화 및 실행을 일원화
    - 기존의 파일 로드 및 리포지토리 초기화 코드를 제거하여 코드 간결화
    - 주석 처리된 테스트용 코드 삭제

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,40 +1,12 @@
 package store;
 
-import store.io.FileDataLoader;
-import store.io.ProductDataLoader;
-import store.io.PromotionDataLoader;
-import store.repository.Repository;
-import store.repository.ProductFileRepository;
-import store.repository.PromotionFileRepository;
-import store.util.FilePathLoader;
-import store.view.OutputView;
-
-import java.io.IOException;
+import store.config.*;
+import store.controller.StoreController;
 
 public class Application {
     public static void main(String[] args) {
-
-        OutputView outputView = new OutputView();
-        outputView.printWelcomeMessage();
-        outputView.printCurrentProductsMessage();
-
-//        try {
-//            FileDataLoader productLoader = new ProductDataLoader();
-//            FileDataLoader promotionLoader = new PromotionDataLoader();
-//
-//            String productFilePath = FilePathLoader.getResourceFilePath("products.md");
-//            String promotionFilePath = FilePathLoader.getResourceFilePath("promotions.md");
-//
-//            Repository<String> productRepository = new ProductFileRepository(productLoader, productFilePath);
-//            Repository<String> promotionRepository = new PromotionFileRepository(promotionLoader, promotionFilePath);
-//
-//            System.out.println("Products:");
-//            productRepository.findAll().forEach(System.out::println);
-//
-//            System.out.println("\nPromotions:");
-//            promotionRepository.findAll().forEach(System.out::println);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        StoreConfig storeConfig = new StoreConfig();
+        StoreController storeController = storeConfig.createStoreController();
+        storeController.run();
     }
 }
